ceff91c8263bf2fdc443100ab790ce3ec0b002d0
SVEND-1 <s5090@inbox.ru>
2025-01-26 12:30:53 +0300

    Почти доделал интерфейс у добавление категории

--- app/src/main/java/com/example/finance/Activity/AddWasteAndIncomeActivity.java
+++ app/src/main/java/com/example/finance/Activity/AddWasteAndIncomeActivity.java
@@ -1,18 +1,42 @@
 package com.example.finance.Activity;
 
+import static androidx.core.view.MenuItemCompat.getTooltipText;
+
+import static com.example.finance.R.color.green;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.finance.Fragment.AddCategoryIncomeFragment;
+import com.example.finance.Fragment.AddCategotyWasteFragment;
 import com.example.finance.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class AddWasteAndIncomeActivity extends AppCompatActivity {
+
+    private TextView _textView;
+    private LinearLayout _lastClickedLinearLayout = null;
+    private TabLayout _tabLayot;
+    private AutoCompleteTextView _description;
+    private GridLayout _gridLayoutWithCategory;
+    private TabItem _tabItemWaste,_tabItemIncome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +48,66 @@ public class AddWasteAndIncomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        ClickTableLayout();
+    }
+    private void init(){
+        _textView = findViewById(R.id.addWasteAndIncomeSumText);
+        _description = findViewById(R.id.addWasteAndIncomeDescriptionET);
+        _tabLayot = findViewById(R.id.addWasteAndIncomeTabLayoutIncomeOrWaste);
+        _tabItemWaste = findViewById(R.id.addWasteAndIncomeTabWaste);
+        _tabItemIncome = findViewById(R.id.addWasteAndIncomeTabIncome);
+    }
+
+
+    public void ClickGetCategory(View v) {
+        if (v instanceof CardView) {
+            handleCardViewClick((CardView) v);
+        } else if (v instanceof LinearLayout) {
+            handleLinearLayoutClick((LinearLayout) v);
+        }
+    }
+
+    private void ClickTableLayout(){
+        _tabLayot.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    private void handleCardViewClick(CardView cardView) {
+        String categoryText = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence tooltipText = cardView.getTooltipText();
+            if (tooltipText != null) {
+                categoryText = tooltipText.toString();
+            }
+        }
+        _textView.setText(categoryText);
+    }
+
+    private void handleLinearLayoutClick(LinearLayout linearLayout) {//Это когда нажимаешь на выбор катерогии
+        // Сначала сбрасываем цвет предыдущего нажатого LinearLayout, если он существует
+        if (_lastClickedLinearLayout != null && _lastClickedLinearLayout != linearLayout) {
+            _lastClickedLinearLayout.setBackgroundColor(getResources().getColor(R.color.white));
+        }
+
+        // Устанавливаем цвет для текущего нажатого LinearLayout
+        linearLayout.setBackgroundColor(getResources().getColor(R.color.green)); // Устанавливаем цвет в зеленый
+
+        // Обновляем ссылку на последний нажатый LinearLayout
+        _lastClickedLinearLayout = linearLayout;
     }
 
 }
---
+++ app/src/main/java/com/example/finance/FINAL.java
@@ -0,0 +1,22 @@
+package com.example.finance;
+
+public class FINAL {
+    public final static String CATEGORY_WASTE_EAT= "Еда";
+    public final static String CATEGORY_WASTE_GROCERY_STORE = "Продуктовый магазин";
+    public final static String CATEGORY_WASTE_CLOTHES = "Одежда";
+    public final static String CATEGORY_WASTE_HOSPITAL = "Больница";
+    public final static String CATEGORY_WASTE_SPORT = "Спорт";
+    public final static String CATEGORY_WASTE_TRANSPORT = "Транспорт";
+    public final static String CATEGORY_WASTE_LEISURE = "Досуг";
+    public final static String CATEGORY_WASTE_OTHER = "Другое";
+
+
+    public final static String CATEGORY_INCOME_SALARY = "Зарплата";
+    public final static String CATEGORY_INCOME_GIFTS = "Подарки";
+    public final static String CATEGORY_INCOME_BANK = "Проценты банка";
+    public final static String CATEGORY_INCOME_PAYMENTS = "Гос. выплаты";
+    public final static String CATEGORY_INCOME_STOCKS = "Акции";
+    public final static String CATEGORY_INCOME_SECURITIES = "Ценные бумаги";
+    public final static String CATEGORY_INCOME_SELL = "Продажа";
+    public final static String CATEGORY_INCOME_OTHER = "Другое";
+}
